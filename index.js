d08ab864654f8f1de66c3e774c1bf7614bc1088b
Shivangi-ch <shivangi.ch.936@gmail.com>
2020-10-01 19:41:38 +0530

    Added welcome message , interactive,animations

---
+++ index.js
@@ -0,0 +1,18 @@
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+anime.timeline({loop: true})
+.add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 70*i
+}).add({
+    targets: '.ml2',
+    opacity: 0,
+    duration: 1200,
+    easing: "easeOutExpo",
+    delay: 600
+});
